741de57ddbd395c3493ff04d4767f57f3a089ebe
Shubham <shubhamvscode@gmail.com>
2023-05-02 11:18:35 +0530

    User Routes cleanup

--- routes/users.js
+++ routes/users.js
@@ -11,22 +11,11 @@ const {
 
 const router = express.Router();
 
-// Route to get the current user
-router.get("/getUser", isLoggedIn, getUser);
-
-// Register route
-router.post("/register", register);
-
-// Login route
-router.post("/login", login);
-
-// Route to update user role
-router.put("/updateRole/:id", updateRole);
-
-// Routes for searching user
-router.post("/search", searchUsers);
-
-// Update a user
-router.put("/updateDetails/:id", isTA, updateDetails);
+router.get("/getUser", isLoggedIn, getUser); // Route to get the current user
+router.post("/register", register); // Register route
+router.post("/login", login); // Login route
+router.put("/updateRole/:id", updateRole); // Route to update user role
+router.post("/search", searchUsers); // Routes for searching user
+router.put("/updateDetails/:id", isTA, updateDetails); // Update a user
 
 module.exports = router;
